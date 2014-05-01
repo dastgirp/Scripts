1126198b274dce03c57f28141cfc4a14b56d8261
Dastgir Pojee <dastgirpojee@rocketmail.com>
2014-05-01 21:05:21 +0530

    Added a New option that enables the logs, if by-chance the script fails to execute, one can send me item_db_conversion.logs for checking..

--- Lua/conf2db/conf2db.bat
+++ Lua/conf2db/conf2db.bat
@@ -3,5 +3,6 @@
 set /p conffile=Enter .Conf FileName: 
 set /p textfile=Enter Output FileName: 
 set /p format=Enter Format("RE",Or see README.md): 
-lua.exe conf2db.lua %conffile% %textfile% "%format%"
+set /p logs=Enables Logs(1 or 0)
+lua.exe conf2db.lua %conffile% %textfile% "%format%" %logs%
 pause

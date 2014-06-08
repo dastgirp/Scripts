3c7bb18f346c4c1880a4710d973ad3b03bb13b80
Dastgir Pojee <dastgirpojee@rocketmail.com>
2014-06-08 21:12:24 +0530

    Added Feature to copy name from iteminfo.lua, and fixed bat file typo

--- Lua/CopyDisplayName/copyname.bat
+++ Lua/CopyDisplayName/copyname.bat
@@ -2,6 +2,7 @@
 set /p range1=Enter Item Range to start item_name copying: 
 set /p range2=Enter Item Range to end item_name copying: 
 set /p conffile=Enter item_db.Conf FileName: 
-set /p textfile=Enter idnum2itemdisplaynametable FileName: 
-lua.exe copyname.lua %range1% %range2% "%conffile%" "%textfile%"
+set /p textfile=Enter idnum2itemdisplaynametable.txt/iteminfo.lua/iteminfo.lub FileName: 
+set /p output=Output File Name: 
+lua.exe source_copyname.lua %range1% %range2% "%textfile%" "%conffile%" "%output%"
 pause

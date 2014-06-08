3a7a3be6218a905dfc6d7492e6dea124695ab7e8
Dastgir Pojee <dastgirpojee@rocketmail.com>
2014-06-08 21:14:42 +0530

    Fixed Bat File

--- Lua/CopyDisplayName/copyname.bat
+++ Lua/CopyDisplayName/copyname.bat
@@ -4,5 +4,5 @@ set /p range2=Enter Item Range to end item_name copying:
 set /p conffile=Enter item_db.Conf FileName: 
 set /p textfile=Enter idnum2itemdisplaynametable.txt/iteminfo.lua/iteminfo.lub FileName: 
 set /p output=Output File Name: 
-lua.exe source_copyname.lua %range1% %range2% "%textfile%" "%conffile%" "%output%"
+lua.exe copyname.lua %range1% %range2% "%textfile%" "%conffile%" "%output%"
 pause

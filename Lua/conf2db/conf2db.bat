481ff335b67a52b41b528ff38e151ffe1c142311
Dastgir <dastgirpojee@rocketmail.com>
2014-07-13 11:36:42 +0530

    Updated ItemDB Conf->TxT to work with new item_db format

--- Lua/conf2db/conf2db.bat
+++ Lua/conf2db/conf2db.bat
@@ -3,6 +3,6 @@
 set /p conffile=Enter .Conf FileName: 
 set /p textfile=Enter Output FileName: 
 set /p format=Enter Format("RE",Or see README.md): 
-set /p logs=Enables Logs(1 or 0)
+set /p logs=Enables Logs(1 or 0): 
 lua.exe conf2db.lua %conffile% %textfile% "%format%" %logs%
 pause

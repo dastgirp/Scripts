dd0d86cb113e23c1508f09f1be51483f452f8237
Dastgir <dastgirpojee@rocketmail.com>
2015-12-23 21:27:27 +0530

    NextGen Scripting(conf2db): [WIP] Expanded tables.lua to support some types(you can add yours), FileSize reduced to 50%

--- Lua/conf2db/table.lua
+++ Lua/conf2db/table.lua
@@ -1,44 +1,57 @@
 -- Comments are Auto Parsed
+
+-- Constants
+typ = {
+	int = 1,
+	bool = 2,
+	hex8 = 3,
+	hex2 = 4,
+	string = 5,
+	group = 6,
+	script = 7,
+}
+
+-- ItemDB
 tbl = {
 	-- ItemDB Fields
-	fields = {
-		'Id',
-		'AegisName',
-		'Name',
-		'Type',
-		'Buy',
-		'Sell',
-		'Weight',
-		'Atk',
-		'Matk',
-		'Def',
-		'Range',
-		'Slots',
-		'Job',
-		'Upper',
-		'Gender',
-		'Loc',
-		'WeaponLv',
-		'EquipLv',
-		'Refine',
-		'View',
-		'BindOnEquip',
-		'BuyingStore',
-		'Delay',
-		'KeepAfterUse',
-			'override',
-			'nodrop',
-			'notrade',
-			'partneroverride',
-			'noselltonpc',
-			'nocart',
-			'nostorage',
-			'nogstorage',
-			'nomail',
-			'noauction',
-			'sitting',
-		'Stack',
-		'Sprite',
+	fields = {	--Names are Formats too
+		{'Id', typ.int },
+		{'AegisName', typ.string },
+		{'Name', typ.string },
+		{'Type', typ.int },
+		{'Buy', typ.int },
+		{'Sell', typ.int },
+		{'Weight', typ.int },
+		{'Atk', typ.int },
+		{'Matk', typ.int },
+		{'Def', typ.int },
+		{'Range', typ.int },
+		{'Slots', typ.int },
+		{'Job', typ.hex8 },
+		{'Upper', typ.hex2 },
+		{'Gender', typ.int },
+		{'Loc', typ.int },
+		{'WeaponLv', typ.int },
+		{'EquipLv', typ.group },
+		{'Refine', typ.bool },
+		{'View', typ.int },
+		{'BindOnEquip', typ.bool },
+		{'BuyingStore', typ.bool },
+		{'Delay', typ.int },
+		{'KeepAfterUse', typ.bool },
+		{	'override', typ.int },
+		{	'nodrop', typ.bool },
+		{	'notrade', typ.bool },
+		{	'partneroverride', typ.bool },
+		{	'noselltonpc', typ.bool },
+		{	'nocart', typ.bool },
+		{	'nostorage', typ.bool },
+		{	'nogstorage', typ.bool },
+		{	'nomail', typ.bool },
+		{	'noauction', typ.bool },
+		{	'sitting', typ.bool },
+		{'Stack', typ.group },
+		{'Sprite', typ.int },
 	},
 	-- [Groups with internal settings]
 	-- Groups have { } 
@@ -49,13 +62,27 @@ tbl = {
 	-- Arrays have [ ]
 	array = {	-- Should be inside fields too
 		-- Format
-		-- { 'FieldName', ValuesCount}
-		{ 'EquipLv', 2 },
-		{ 'Stack', 2 },
+		-- { 'FieldName', ValuesCount, {Type1, Type2}}
+		{ 
+			'EquipLv',
+			2,
+			{
+				typ.int,
+				typ.int
+			}
+		},
+		{ 
+			'Stack',
+			2,
+			{
+				typ.int,
+				typ.int
+			}
+		},
 		
 	},
 	-- script have <" ">
-	script = {
+	script = {	--Treated as ScriptType
 		'Script',
 		'OnEquipScript',
 		'OnUnequipScript' 

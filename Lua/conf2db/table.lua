c6c4fce410f0702892462216bc58b02d88c2974f
Dastgir <dastgirpojee@rocketmail.com>
2016-03-30 13:01:19 +0530

    Updated conf2db: 	Now Supports Job Field

--- Lua/conf2db/table.lua
+++ Lua/conf2db/table.lua
@@ -2,17 +2,18 @@
 
 -- Constants
 typ = {
-	int = 1,
-	bool = 2,
-	hex8 = 3,
-	hex2 = 4,
-	string = 5,
-	group = 6,
-	script = 7,
+	int = 1,	-- Integer
+	bool = 2,	-- Boolean
+	hex = 3,	-- Hex 
+	value = 4,	-- Assign Value from value_s table
+	string = 5,	-- String
+	group = 6,	-- LibConfig Group
+	script = 7,	-- Script Format
 }
 
 -- ItemDB
 tbl = {
+	item_tbl_name = "item_db",	-- See at start of item_db, the initial line determine's this.
 	-- ItemDB Fields
 	fields = {	--Names are Formats too
 		{'Id', typ.int },
@@ -27,8 +28,8 @@ tbl = {
 		{'Def', typ.int },
 		{'Range', typ.int },
 		{'Slots', typ.int },
-		{'Job', typ.hex8 },
-		{'Upper', typ.hex2 },
+		{'Job', typ.value },
+		{'Upper', {typ.hex, 2} },
 		{'Gender', typ.int },
 		{'Loc', typ.int },
 		{'WeaponLv', typ.int },
@@ -36,6 +37,7 @@ tbl = {
 		{'Refine', typ.bool },
 		{'View', typ.int },
 		{'BindOnEquip', typ.bool },
+		{'ForceSerial', typ.bool },
 		{'BuyingStore', typ.bool },
 		{'Delay', typ.int },
 		{'KeepAfterUse', typ.bool },
@@ -53,11 +55,48 @@ tbl = {
 		{'Stack', typ.group },
 		{'Sprite', typ.int },
 	},
+	
+	value_s = {
+		-- Job with its Hex Code
+		-- Only Bool Supported till now
+		{ 'Novice',            0x00000001, typ.bool },
+		{ 'Swordsman',         0x00000002, typ.bool },
+		{ 'Magician',          0x00000004, typ.bool },
+		{ 'Archer',            0x00000008, typ.bool },
+		{ 'Acolyte',           0x00000010, typ.bool },
+		{ 'Merchant',          0x00000020, typ.bool },
+		{ 'Thief',             0x00000040, typ.bool },
+		{ 'Knight',            0x00000080, typ.bool },
+		{ 'Priest',            0x00000100, typ.bool },
+		{ 'Wizard',            0x00000200, typ.bool },
+		{ 'Blacksmith',        0x00000400, typ.bool },
+		{ 'Hunter',            0x00000800, typ.bool },
+		{ 'Assassin',          0x00002000, typ.bool },
+		{ 'Crusader',          0x00004000, typ.bool },
+		{ 'Monk',              0x00008000, typ.bool },
+		{ 'Sage',              0x00010000, typ.bool },
+		{ 'Rogue',             0x00020000, typ.bool },
+		{ 'Alchemist',         0x00040000, typ.bool },
+		{ 'Bard',              0x00080000, typ.bool },
+		{ 'Taekwon',           0x00200000, typ.bool },
+		{ 'Star_Gladiator',    0x00400000, typ.bool },
+		{ 'Soul_Linker',       0x00800000, typ.bool },
+		{ 'Gunslinger',        0x01000000, typ.bool },
+		{ 'Ninja',             0x02000000, typ.bool },
+		{ 'Gangsi',            0x04000000, typ.bool },
+		{ 'Death_Knight',      0x08000000, typ.bool },
+		{ 'Dark_Collector',    0x10000000, typ.bool },
+		{ 'Kagerou',           0x20000000, typ.bool },
+		{ 'Rebellion',         0x40000000, typ.bool },
+		{ 'Summoner',          0x80000000, typ.bool },
+		{ 'All',               0xFFFFFFFF, typ.bool },
+	},
 	-- [Groups with internal settings]
 	-- Groups have { } 
 	group_s = {
 		'Nouse',
 		'Trade',
+		'Job',
 	},
 	-- Arrays have [ ]
 	array = {	-- Should be inside fields too

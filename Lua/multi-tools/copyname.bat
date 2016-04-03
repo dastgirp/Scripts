f95dc8dd4a31226aee0ababef84a702f94664c6f
Dastgir <dastgirpojee@rocketmail.com>
2016-04-03 21:22:20 +0530

    Fixes Issue #6, Typo errors. There was mistake in program(recognizing debug/production profile) which was causing some variable to be replaced with 0.

--- Lua/multi-tools/copyname.bat
+++ Lua/multi-tools/copyname.bat
@@ -4,5 +4,5 @@ set /p range2=Enter Item Range to end item_name copying:
 set /p conffile=Enter item_db.Conf FileName: 
 set /p textfile=Enter idnum2itemdisplaynametable.txt/iteminfo.lua/iteminfo.lub FileName: 
 set /p output=Output File Name: 
-lua53i.exe multi-tools.lub copydisplayname %range1% %range2% "%textfile%" "%conffile%" "%output%"
+lua53.exe multi-tools.lub copydisplayname %range1% %range2% %textfile% %conffile% %output%
 pause

9e0ec16df01608be457982c8f6b69aad1052d081
Dastgir Pojee <dastgirp@gmail.com>
2017-10-07 13:17:07 +0530

    mergeidnum: 	- Added support for rA's item_db.txt

--- Lua/multi-tools/mergeidnum.bat
+++ Lua/multi-tools/mergeidnum.bat
@@ -2,6 +2,6 @@
 :Start
 set /p inputfolder=Enter Input Folder: 
 set /p itemInfo=Enter Output FileName(itemInfo.lub): 
-set /p itemDb=Enter item_db.conf Name(item_db.conf/null): 
+set /p itemDb=Enter item_db.conf Name(item_db.conf/item_db.txt/null): 
 lua53.exe multi-tools.lub mergeidnum "%inputfolder%" "%itemInfo%" "%itemDb%"
 pause
